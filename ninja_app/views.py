97505a987f366e779938e63a2cccf1bd638615e5
gonzalo <gonzaloibarra8@gmail.com>
2021-08-04 23:55:31 -0400

    actualizacion archivos

--- ninja_app/views.py
+++ ninja_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render, HttpResponse
 import random
+from time import gmtime, strftime,localtime
 def index(request):
     return render(request, 'index2.html')
 
@@ -12,29 +13,29 @@ def money(request):
         if request.POST['ninja'] == 'Farm':
             a =random.randint(10,20)
             request.session['cont'] += a
-            diccionario = { 'texto': f'¡¡¡Ganaste {a} monedas de oro!!!', 'color': 'verde' }
+            diccionario = { 'texto': f'¡¡¡Ganaste {a} monedas de oro!!!', 'color': 'verde', 'date': strftime("%H:%M %p", localtime()) }
             request.session['list'].append(diccionario)
         if request.POST['ninja'] == 'Cave':
             a = random.randint(5,10)
             request.session['cont'] += a
-            diccionario = { 'texto': f'¡¡¡Ganaste {a} monedas de oro!!!', 'color': 'verde' }
+            diccionario = { 'texto': f'¡¡¡Ganaste {a} monedas de oro!!!', 'color': 'verde', 'date': strftime("%H:%M %p", localtime()) }
             request.session['list'].append(diccionario)
         if request.POST['ninja'] == 'House':
             a = random.randint(2,5)
             request.session['cont'] += a
-            diccionario = { 'texto': f'¡¡¡Ganaste {a} monedas de oro!!!', 'color': 'verde' }
+            diccionario = { 'texto': f'¡¡¡Ganaste {a} monedas de oro!!!', 'color': 'verde', 'date': strftime("%H:%M %p", localtime()) }
             request.session['list'].append(diccionario)
         if request.POST['ninja'] == 'Casino':
             a = random.randint(1,2)
             if a == 1:
                 b = random.randint(50,60)
                 request.session['cont'] += b
-                diccionario = { 'texto': f'¡¡¡Ganaste {b} monedas de oro!!!', 'color': 'verde' }
+                diccionario = { 'texto': f'¡¡¡Ganaste {b} monedas de oro!!!', 'color': 'verde', 'date': strftime("%H:%M %p", localtime()) }
                 request.session['list'].append(diccionario)
             else:
                 b = random.randint(50,60)
                 request.session['cont'] -= b
-                diccionario = { 'texto': f'¡¡¡Perdiste {b} monedas de oro!!!', 'color': 'rojo' }
+                diccionario = { 'texto': f'¡¡¡Perdiste {b} monedas de oro!!!', 'color': 'rojo', 'date': strftime("%H:%M %p", localtime()) }
                 request.session['list'].append(diccionario)
 
     else:
